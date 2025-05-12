fbb3402396a1b681f4f835f5dc840a950ae86c9c
daniel.castellano.serrano <daniel.castellano.serrano@estudiantat.upc.edu>
2025-05-12 16:31:48 +0200

    TG-129-Directions

--- src/Dto/RouteResponseDto.cs
+++ src/Dto/RouteResponseDto.cs
@@ -6,4 +6,6 @@ public class RouteResponseDto
   public double Duration { get; set; }
   public List<double[]> Geometry { get; set; } = new();
   public List<RouteInstructionDto> Instructions { get; set; }
+  public string? OriginStreetName { get; set; }
+  public string? DestinationStreetName { get; set; }
 }
--- src/Dto/UbicationInfoDto.cs
+++ src/Dto/UbicationInfoDto.cs
@@ -2,7 +2,7 @@
 public class UbicationInfoDto
 {
   public required int UbicationId { get; set; }
-  public required string Username { get; set; }
+  public required string UserEmail { get; set; }
   public required string StationType { get; set; }
   public int? Valoration { get; set; }
   public string? Comment { get; set; }
