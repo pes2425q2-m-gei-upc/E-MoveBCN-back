feb89fb883e25e00984c50db7961ff28577bbc02
DanielEspinalt <162478401+DanielEspinalt@users.noreply.github.com>
2025-05-23 17:30:29 +0200

    hotfix/Issue

--- src/Entity/Bicing/StationEntity.cs
+++ src/Entity/Bicing/StationEntity.cs
@@ -4,7 +4,7 @@ public class StationEntity
 {
 
   public required string StationId { get; set; }
-  public string StationLabel { get; set; }
+  public string? StationLabel { get; set; }
   public required double StationLatitude { get; set; }
   public required double StationLongitude { get; set; }
   public required string LocationId { get; set; }
